6ce40899f537fa648adb96f5a1dd36c48615f516
matthewjrogers <matthew.rogers09@gmail.com>
2022-06-28 10:31:53 -0400

    file io tests

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,29 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if !(len(d) <= 52) {
-		t.Error("Too many cards. Expected deck length <= 52, got", len(d))
+		t.Errorf("Too many cards. Expected deck length <= 52, got %v", len(d))
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	tp := "_decktesting"
+	os.Remove(tp)
+	deck := newDeck()
+	deck.saveToFile(tp)
+
+	loadedDeck := newDeckFromFile(tp)
+
+	if !(len(loadedDeck) <= 52) {
+		t.Errorf("Too many cards. Expected deck length <= 52, got %v", len(loadedDeck))
+	}
+
+	os.Remove(tp)
 }
