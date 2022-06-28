1a7057beb29a4276dc722d8a64a060ee1831e587
matthewjrogers <matthew.rogers09@gmail.com>
2022-06-28 09:34:28 -0400

    basic shuffle

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -49,6 +50,7 @@ func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
+// create a new deck from a file
 func newDeckFromFile(filename string) deck {
 	bs, err := ioutil.ReadFile(filename)
 
@@ -57,4 +59,11 @@ func newDeckFromFile(filename string) deck {
 		os.Exit(1)
 	}
 	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		elem := rand.Intn(len(d) - 1)
+		d[i], d[elem] = d[elem], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 	cards := newDeckFromFile("my_cards")
-
+	cards.shuffle()
 	//hand, cards := deal(cards, 3)
 	//hand.print()
 	cards.print()
